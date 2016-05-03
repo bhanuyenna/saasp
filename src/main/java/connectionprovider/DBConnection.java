452df957ff5a03347fd231c67c67d00198b26f22
bhanuyenna <yennabhanu1@gmail.com>
2016-05-02 22:10:55 -0400

    added the online postgres

--- src/main/java/connectionprovider/DBConnection.java
+++ src/main/java/connectionprovider/DBConnection.java
@@ -8,81 +8,80 @@ import java.sql.Connection;
 import java.sql.SQLException;
 import java.sql.Statement;
 
+public class DBConnection {
+	public static Connection getConnection() throws URISyntaxException,	SQLException {
+URI dbUri = new URI(System.getenv("DATABASE_URL"));
+
+String username = dbUri.getUserInfo().split(":")[0];
+String password = dbUri.getUserInfo().split(":")[1];
+try {
+	Class.forName("org.postgresql.Driver");
+} catch (ClassNotFoundException e) {
+	e.printStackTrace();
+}
+String dbUrl = "jdbc:postgresql://"
+		+ dbUri.getHost()
+		+ ':'
+		+ dbUri.getPort()
+		+ dbUri.getPath()
+		+ "?sslmode=require&ssl=true&sslfactory=org.postgresql.ssl.NonValidatingFactory";
+System.out.println(dbUrl);
+System.out.println("Database Connected");
+return DriverManager.getConnection(dbUrl, username, password);
+}
+
+public static void main(String[] args) {
+try {
+	Connection connection = getConnection();
+	Statement stmt = connection.createStatement();
+////	stmt.executeUpdate("INSERT INTO GOODS(THING) VALUES('My cool Item');");
+////	ResultSet rs = stmt.executeQuery("SELECT Thing FROM Goods");
+////	while (rs.next()) {
+////		System.out.println("Thing: " + rs.getString("thing"));
+////	}
+} catch (URISyntaxException e) {
+	e.printStackTrace();
+} catch (SQLException e) {
+	e.printStackTrace();
+		}
+	}
+	}
+
+	
 //public class DBConnection {
-//	public static Connection getConnection() throws URISyntaxException,	SQLException {
-//URI dbUri = new URI(System.getenv("DATABASE_URL"));
-//
-//String username = dbUri.getUserInfo().split(":")[0];
-//String password = dbUri.getUserInfo().split(":")[1];
-//try {
-//	Class.forName("org.postgresql.Driver");
-//} catch (ClassNotFoundException e) {
-//	e.printStackTrace();
-//}
-//String dbUrl = "jdbc:postgresql://"
-//		+ dbUri.getHost()
-//		+ ':'
-//		+ dbUri.getPort()
-//		+ dbUri.getPath()
-//		+ "jexnubdd:8St_JK_3TYYcIjLLusP8uBwQrFWHNpIW@pellefant.db.elephantsql.com:5432/jexnubdd";
-//System.out.println(dbUrl);
-//System.out.println("Database Connected");
-//return DriverManager.getConnection(dbUrl, username, password);
-//}
-//
-//public static void main(String[] args) {
-//try {
-//	Connection connection = getConnection();
-//	Statement stmt = connection.createStatement();
-//	stmt.executeUpdate("INSERT INTO GOODS(THING) VALUES('My cool Item');");
-//	ResultSet rs = stmt.executeQuery("SELECT Thing FROM Goods");
-//	while (rs.next()) {
-//		System.out.println("Thing: " + rs.getString("thing"));
+//	
+//	public static Connection getConnection() throws Exception	{
+//	try
+//	{
+//	String connectionURL = "jdbc:postgresql://localhost:5432/mydb";
+//	Connection connection = null;
+//	Class.forName("org.postgresql.Driver").newInstance();
+//	connection = DriverManager.getConnection(connectionURL,"postgres",
+//			"root");
+//	System.out.println("Database Connected");
+//	return connection;
+//	} catch (Exception e)
+//	{
+//	throw e;
 //	}
-//} catch (URISyntaxException e) {
-//	e.printStackTrace();
-//} catch (SQLException e) {
-//	e.printStackTrace();
+//	}
+//	
+//
+//	public static void main(String[] args) {
+//		try {
+//			Connection connection = getConnection();
+//			System.out.println("Database connected successfully");
+//			
+//		} catch (URISyntaxException e) {
+//			e.printStackTrace();
+//		} catch (SQLException e) {
+//			e.printStackTrace();
+//		}
+//		
+//		catch(Exception e){
+//			System.out.println("Exception is:"+e);
+//		
 //		}
 //	}
-//	}
+// }
 //	
-//	
-public class DBConnection {
-	
-	public static Connection getConnection() throws Exception
-	{
-	try
-	{
-	String connectionURL = "jdbc:postgresql://localhost:5432/mydb";
-	Connection connection = null;
-	Class.forName("org.postgresql.Driver").newInstance();
-	connection = DriverManager.getConnection(connectionURL,"postgres",
-			"root");
-	System.out.println("Database Connected");
-	return connection;
-	} catch (Exception e)
-	{
-	throw e;
-	}
-	}
-	
-	
-	public static void main(String[] args) {
-		try {
-			Connection connection = getConnection();
-			System.out.println("Database connected successfully");
-			
-		} catch (URISyntaxException e) {
-			e.printStackTrace();
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-		
-		catch(Exception e){
-			System.out.println("Exception is:"+e);
-		
-		}
-	}
-  }
-	
